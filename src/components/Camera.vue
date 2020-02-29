73cf34c994353a4d7f2cc2bab862e1bf7df29116
ctufaro <ctufaro@gmail.com>
2020-02-29 17:31:00 -0500

    Shit

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,15 +1,25 @@
 <template>
     <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
         <v-card>
-            <v-toolbar dense flat>
-                <v-btn icon x-large color="black" @click="closeCamera()">
+            <v-app-bar flat color="black" app>
+                <v-btn icon x-large color="white" @click="closeCamera()">
                     <v-icon>far fa-times-circle</v-icon>
                 </v-btn>
-            </v-toolbar>
-            <div id="camera"></div>
-            <v-bottom-navigation fixed>
+            </v-app-bar>
+            <video id="gum-local" autoplay playsinline></video>
+            <v-bottom-navigation app background-color="black">
                 <v-btn icon x-large @click.prevent="">
-                    <v-icon color="black">fas fa-camera</v-icon>
+                    <v-badge color="pink" :content="this.count" v-show="this.count>0">
+                        <canvas></canvas>
+                    </v-badge>
+                </v-btn>
+                <v-spacer />
+                <v-btn icon x-large @click.prevent="snap()">
+                    <v-icon color="white">fa fa-camera</v-icon>
+                </v-btn>
+                <v-spacer />
+                <v-btn icon x-large @click.prevent="">
+                    <v-icon color="white">far fa-check-square</v-icon>
                 </v-btn>
             </v-bottom-navigation>
         </v-card>
@@ -17,35 +27,37 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+import {
+    mapMutations
+} from 'vuex'
 
 export default {
     data: () => ({
-        dialog: false
+        dialog: false,
+        count:0
     }),
     methods: {
         ...mapMutations(['insertMessage']),
-        openCamera() {
+        async openCamera() {
             this.dialog = true;
             this.insertMessage('opening camera');
             // Prefer camera resolution nearest to 1280x720.
             var constraints = {
-                audio: true,
-                video: {
-                    width: 1280,
-                    height: 720,
-                    facingMode: { exact: "environment" }
-                }
+                //video: { facingMode: { exact: "environment" } }, //iphone
+                video:true, //laptop            
+                audio:false
             };
             this.insertMessage('navigator.mediaDevices.getUserMedia');
             var self = this;
-            navigator.mediaDevices.getUserMedia(constraints)
+            await navigator.mediaDevices.getUserMedia(constraints)
                 .then(function (mediaStream) {
-                    var video = document.querySelector("#camera");
+                    //var video = document.querySelector("#video");
+                    const video = document.querySelector('video');
+                    const videoTracks = mediaStream.getVideoTracks();
+                    console.log('Got stream with constraints:', constraints);
+                    console.log(`Using video device: ${videoTracks[0].label}`);
+                    window.stream = mediaStream; // make variable available to browser console
                     video.srcObject = mediaStream;
-                    video.onloadedmetadata = function () {
-                        video.play();
-                    };
                 })
                 .catch(function (err) {
                     self.insertMessage(err.name + ": " + err.message);
@@ -54,10 +66,33 @@ export default {
         closeCamera() {
             this.dialog = false;
             this.insertMessage('closing camera');
+            const canvas = window.canvas = document.querySelector('canvas');
+            var context = canvas.getContext('2d');
+            context.clearRect(0,0, canvas.width, canvas.height);
+            this.count = 0;
+        },
+        snap(){
+            const video = window.video = document.querySelector('video');
+            const canvas = window.canvas = document.querySelector('canvas');
+            canvas.width = 50;
+            canvas.height = 50;
+            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+            this.count++;
+            //this.upload();
+        },
+        upload(){
+            var self = this;
+            const canvas = document.querySelector('canvas');
+            canvas.toBlob(function(blob) {
+                const formData = new FormData();
+                formData.append('my-file', blob, 'filename.png');
+                // Post via axios or other transport method
+                self.axios.post('https://localhost:44302/api/ImageUpload', formData);
+            });
         }
     },
-    mounted(){
-        
+    mounted() {
+
     }
 }
 </script>
