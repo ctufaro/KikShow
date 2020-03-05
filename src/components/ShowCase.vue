8fca4f8ec7e94f9efd27741c833407777e8e5290
ctufaro <ctufaro@gmail.com>
2020-03-04 22:39:40 -0500

    Shit

--- src/components/ShowCase.vue
+++ src/components/ShowCase.vue
@@ -1,27 +1,24 @@
 <template>
     <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-        <v-card>      
-            <v-img :src="anigif" @load="imageloaded"></v-img> 
-            <v-dialog v-model="loadingmsg" hide-overlay persistent width="300">
+        <v-img :src="anigif" @load="imageloaded"></v-img>
+        <v-dialog v-model="loadingmsg" hide-overlay persistent width="300">
             <v-card color="primary" dark>
                 <v-card-text class="pt-3">
                     So close to being done
                     <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
                 </v-card-text>
-                </v-card>
-            </v-dialog>      
-            <v-bottom-navigation app background-color="black">
-                <v-btn @click.prevent="dialog=!dialog">
-                    <v-icon color="white">far fa-thumbs-up</v-icon>
-                </v-btn>                
-                <v-spacer />
-                <v-btn @click.prevent="trash">
-                    <v-icon color="white">far fa-trash-alt</v-icon>
-                </v-btn>                
-            </v-bottom-navigation>
-        </v-card>        
+            </v-card>
+        </v-dialog>
+        <v-bottom-navigation app background-color="black">
+            <v-btn @click.prevent="dialog=!dialog">
+                <v-icon color="white">far fa-thumbs-up</v-icon>
+            </v-btn>
+            <v-spacer />
+            <v-btn @click.prevent="trash">
+                <v-icon color="white">far fa-trash-alt</v-icon>
+            </v-btn>
+        </v-bottom-navigation>
     </v-dialog>
-    
 </template>
 
 <script>
@@ -57,3 +54,8 @@ export default {
     }
 }
 </script>
+<style>
+.v-dialog--fullscreen {
+    background-color: white;
+}
+</style>
